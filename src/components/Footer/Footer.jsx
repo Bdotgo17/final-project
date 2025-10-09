a885deab7f163608feede10c46fcb9ffea39f328
Baruc Gomez <bdotgo@hotmail.com>
2025-10-09 17:46:13 -0500

    messing with footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <div className="footer-content">
+      <div className="footer-right">
         <div className="footer-links">
           <a href="/" className="footer-link">
             Home
@@ -35,22 +35,11 @@ function Footer() {
             <img src={linkedinLogo} alt="LinkedIn" className="footer-icon" />
           </a>
         </div>
-      </div>
-      <div className="footer-copyright">
-        ©2025 Supersite, Powered by News API
+        <div className="footer-copyright">
+          ©2025 Supersite, Powered by News API
+        </div>
       </div>
     </footer>
-    // <footer className="footer">
-    //   <div className="footer-left">©2025 Supersite, Powered by News API</div>
-    //   <div className="footer-right">
-    //     <a href="/" className="footer-link">
-    //       Home
-    //     </a>
-    //     <span className="footer-link">TripleTen</span>
-    //     <img src={githubLogo} alt="GitHub" className="footer-icon" />
-    //     <img src={linkedinLogo} alt="LinkedIn" className="footer-icon" />
-    //   </div>
-    // </footer>
   );
 }
 
