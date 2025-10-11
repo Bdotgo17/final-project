09064ec6c73384df17b1bc56d5e8fa52420cad1f
Baruc Gomez <bdotgo@hotmail.com>
2025-10-11 16:11:09 -0500

    updated browserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 import { HashRouter as Router } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Router>
+    <BrowserRouter basename="/final-project">
       <App />
-    </Router>   
-  </StrictMode>,
-)
+    </BrowserRouter>
+  </StrictMode>
+);
