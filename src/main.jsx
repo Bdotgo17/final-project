1d8b2e314caec0196e66ff8b3fd9f4d3d1af239b
Baruc Gomez <bdotgo@hotmail.com>
2025-10-19 17:31:23 -0500

    trying for publish

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./components/App/App";
 import "./index.css";
-import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </StrictMode>
+  <BrowserRouter basename="/final-project">
+    <App />
+  </BrowserRouter>
 );
